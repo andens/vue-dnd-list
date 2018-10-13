6b5724e0a087fda15607067bd3000d8b9c1198f7
Andreas Flöjt <andenzz@hotmail.com>
2018-10-13 12:57:44 +0200

    Basic shell that renders list items using slots.

---
+++ README.md
@@ -0,0 +1,1 @@
+The items that are used in the slot must specify the `key` property because they are internally used inside a `transition-group`.
---
+++ src/SortableContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <transition-group tag="div">
+    <slot v-for="(listItem, index) in value" v-bind="{listItem, index}" />
+  </transition-group>
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: Array, required: true },
+  },
+}
+</script>
