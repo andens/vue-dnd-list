ef949819b3c1607475aeed9c01f6a8e5eefb11ad
Andreas Flöjt <andenzz@hotmail.com>
2018-10-14 20:04:53 +0200

    Calculate the helper position when sorting.

--- src/SortableContainer.vue
+++ src/SortableContainer.vue
@@ -69,7 +69,8 @@ export default {
     settling: false,
     helperTranslation: { x: 0, y: 0 },
     activationTimer: null, // Tracker for when `activationDelay` is used
-    startPosition: { x: 0, y: 0 }, // Tracker for when `activationDistance` is used
+    startPosition: { x: 0, y: 0 }, // Mouse position at the time of activation.
+    helperStartPosition: { x: 0, y: 0 }, // Offset of the sort item when sorting is activated.
     nodeTracker: new NodeTracker(),
   }),
 
@@ -101,6 +102,14 @@ export default {
       window.addEventListener("mousemove", this.handleSortMove, true);
       window.addEventListener("mouseup", this.handleSortEnd, true);
 
+      // If no particular constraint is set, this is the proper start position.
+      // If using drag activation, this is used as the offset base to determine
+      // whether the constraint has been met. Delay activation overwrites this
+      // on mouse moves, but requires this value as a default if the mouse is
+      // not moved during the countdown.
+      this.startPosition.x = e.pageX;
+      this.startPosition.y = e.pageY;
+
       // If an activation delay is set, wait before entering the sorting phase.
       if (this.activationDelay > 0) {
         this.activationTimer = setTimeout(
@@ -108,13 +117,8 @@ export default {
           this.activationDelay
         );
       }
-      // Set a frame of reference for when activation distance is used.
-      else if (this.activationDistance > 0) {
-        this.startPosition.x = e.pageX;
-        this.startPosition.y = e.pageY;
-      }
       // Neither activation delay nor -distance is used: activate immediately
-      else {
+      else if (this.activationDistance === 0) {
         this.activateSorting();
       }
     },
@@ -122,15 +126,24 @@ export default {
     handleSortMove(e) {
       e.stopPropagation();
 
+      // Track the mouse position so that the drag offset is later calculated
+      // from the mouse position at the time of activation.
+      if (!this.sorting && this.activationDelay > 0) {
+        this.startPosition.x = e.pageX;
+        this.startPosition.y = e.pageY;
+      }
       // Sorting has not begun and we are not waiting for an activation delay.
       // This is when drag distance is used to activate sorting.
-      if (!this.sorting && this.activationDelay === 0) {
+      else if (!this.sorting && this.activationDelay === 0) {
         this.checkActivationDistanceConstraint(e);
       }
 
       if (!this.sorting) {
         return;
       }
+
+      this.helperTranslation.x = this.helperStartPosition.x + (e.pageX - this.startPosition.x);
+      this.helperTranslation.y = this.helperStartPosition.y + (e.pageY - this.startPosition.y);
     },
 
     handleSortEnd(e) {
@@ -172,14 +185,19 @@ export default {
       const distSq = delta.x * delta.x + delta.y * delta.y;
 
       if (distSq > this.activationDistance * this.activationDistance) {
+        // Use the current mouse position at the time of activation.
+        this.startPosition.x = e.pageX;
+        this.startPosition.y = e.pageY;
         this.activateSorting();
       }
     },
 
     activateSorting() {
       const sortNode = this.nodeTracker.getNodes()[this.sortIndex];
-      this.helperTranslation.x = sortNode.offsetLeft;
-      this.helperTranslation.y = sortNode.offsetTop;
+      this.helperStartPosition.x = sortNode.offsetLeft;
+      this.helperStartPosition.y = sortNode.offsetTop;
+      this.helperTranslation.x = this.helperStartPosition.x;
+      this.helperTranslation.y = this.helperStartPosition.y;
       this.sorting = true;
     },
 
